95d705676e3fb06ebe67c40a8829c9e23e0b3fde
Ganesh C K M <ckmganesh2000@gmail.com>
2021-12-27 00:23:27 +0530

    Time: 16 ms (86.02%), Space: 39.2 MB (54.68%) - LeetHub

---
+++ sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -0,0 +1,69 @@
+class Solution {
+    public long sumSubarrayMins(int[] A) {
+        int len = A.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] left = new int[len];
+        int[] right = new int[len];
+        for(int i = 0; i < A.length; i++) {
+            left[i] = i + 1;
+            right[i] = len - i;
+        }
+        // previous less element
+        for(int i = 0; i < len; i++){
+            while(!stack.isEmpty() && A[stack.peek()] > A[i]) {
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ? i + 1 : i - stack.peek();
+            stack.push(i);
+        }
+        //next less element
+        stack = new Stack<>();
+        for(int i = 0; i < len; i++){
+            while(!stack.isEmpty() && A[stack.peek()] > A[i]) {
+                right[stack.peek()] = i - stack.peek();
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        long ans = 0;
+        for(int i = 0; i < len; i++) {
+            ans = (ans + (long)A[i] * left[i] * right[i]);
+        }
+        return ans;
+    }
+    public long sumSubarrayMaxs(int[] A) {
+        int len = A.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] left = new int[len];
+        int[] right = new int[len];
+        for(int i = 0; i < A.length; i++) {
+            left[i] = i + 1;
+            right[i] = len - i;
+        }
+        // previous great element
+        for(int i = 0; i < len; i++){
+            while(!stack.isEmpty() && A[stack.peek()] < A[i]) {
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ? i + 1 : i - stack.peek();
+            stack.push(i);
+        }
+        //next great element
+        stack = new Stack<>();
+        for(int i = 0; i < len; i++){
+            while(!stack.isEmpty() && A[stack.peek()] < A[i]) {
+                right[stack.peek()] = i - stack.peek();
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        long ans = 0;
+        for(int i = 0; i < len; i++) {
+            ans = (ans + (long)A[i] * left[i] * right[i]);
+        }
+        return ans;
+    }
+    public long subArrayRanges(int[] nums) {
+        return sumSubarrayMaxs(nums)-sumSubarrayMins(nums);
+    }
+}
