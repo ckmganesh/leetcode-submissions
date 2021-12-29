42586ef6503d37e63b4b0d8627c84f745738cb48
Ganesh C K M <ckmganesh2000@gmail.com>
2021-12-30 00:15:55 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (94.09%) - LeetHub

--- populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -23,22 +23,36 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        if(root==null) return root;
-        Queue<Node> queue = new LinkedList();
-        queue.add(root);
+//         if(root==null) return root;
+//         Queue<Node> queue = new LinkedList();
+//         queue.add(root);
       
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            for(int i=0;i<size;i++){
-                Node top = queue.poll();
-                if(i==size-1)
-                    top.next=null;
-                else
-                    top.next = queue.peek();
-                if(top.left!=null) queue.add(top.left);
-                if(top.right!=null) queue.add(top.right);
-            }
+//         while(!queue.isEmpty()){
+//             int size = queue.size();
+//             for(int i=0;i<size;i++){
+//                 Node top = queue.poll();
+//                 if(i==size-1)
+//                     top.next=null;
+//                 else
+//                     top.next = queue.peek();
+//                 if(top.left!=null) queue.add(top.left);
+//                 if(top.right!=null) queue.add(top.right);
+//             }
            
+//         }
+//         return root;
+        
+        if(root==null) return root;
+        Node current=root;
+        Node nextleftMost=null;
+        while(current.left!=null){
+            nextleftMost = current.left;
+            while(current!=null){
+                current.left.next=current.right;
+                current.right.next=current.next==null?null:current.next.left;
+                current=current.next;
+            }
+            current=nextleftMost;
         }
         return root;
     }
