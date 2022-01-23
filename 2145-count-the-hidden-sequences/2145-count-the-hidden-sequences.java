920cabdda3308fbbc3e9aeb4a8ff0ca6fd88afab
Ganesh C K M <ckmganesh2000@gmail.com>
2022-01-23 16:03:58 +0530

    Time: 30 ms (40.00%), Space: 57.7 MB (20.00%) - LeetHub

---
+++ 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numberOfArrays(int[] diff, int lower, int upper) {
+        List<Integer> result = new ArrayList();
+        result.add(lower);
+        int min=lower,max=lower;
+        for(int i=0;i<diff.length;i++){
+            result.add(result.get(result.size()-1)+diff[i]);
+            min=Math.min(min,result.get(result.size()-1));
+            max=Math.max(max,result.get(result.size()-1));
+        }
+        int liftValue = lower-min;
+        for(int i=0;i<result.size();i++){
+            result.set(i,result.get(i)+liftValue);
+        }
+        for(int a:result){
+            if(a>upper||a<lower) return 0;
+        }
+        int liftValueUp =  (upper-max);
+        return liftValueUp-liftValue+1;
+        
+    }
+}
